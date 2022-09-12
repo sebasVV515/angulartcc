1864cb71734045f070b2760ee25d201411118083
PsychoKramps <Fraiden70@gmail.com>
2022-09-12 11:51:23 -0500

    se crean componentes

---
+++ src/app/componente11/componente11.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente11',
+  templateUrl: './componente11.component.html',
+  styleUrls: ['./componente11.component.css']
+})
+export class Componente11Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/componente14/componente14.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente14',
+  templateUrl: './componente14.component.html',
+  styleUrls: ['./componente14.component.css']
+})
+export class Componente14Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
